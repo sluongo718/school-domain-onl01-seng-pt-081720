5c5dec357abca5582d0d3a6bb59ddabc1b2b5651
sam l <sluongo718@yahoo.com>
2020-09-02 23:08:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  
+  attr_reader  :roster
+  
+  
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+    
+    
+  end
+  
+  def add_student(add_student, level)
+    
+    if roster[level] == nil #if roster does not have this level 
+      roster[level] = [add_student] #then add the level to the roster with student
+    else
+    end
+  end
+  
+  
+end
+
