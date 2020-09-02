622b6465f965d94e9ec4b119a9fe21e121909d51
sam l <sluongo718@yahoo.com>
2020-09-02 23:29:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -14,14 +14,14 @@ class School
   end
   
   def add_student(student, level)
-    binding.pry
+    
     
     if roster[level] == nil #if roster does not have this level 
       roster[level] = [student] #then add the level to the roster with student
     else
     end
   end
-  
+  roster
   
 end
 
