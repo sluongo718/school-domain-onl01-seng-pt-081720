a599fd0f3377100eb5222fcbce432c173d4a7698
sam l <sluongo718@yahoo.com>
2020-09-03 00:22:18 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,46 @@
-# code here!
+require "pry"
+
+class School
+  
+  attr_reader  :roster
+  
+  
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+    
+    
+  end
+  
+  def add_student(student, level)
+    
+    
+    if roster[level] == nil #if roster does not have this level 
+      roster[level] = [student] #then add the level to the roster with student
+    else 
+        roster[level] << student
+      
+    end
+    roster
+  end
+  
+  def grade(student_grade)
+    roster[student_grade]
+  end
+  
+ def sort 
+  new_hash = {}
+  roster.each do |grade, student| 
+    new_hash[grade] = student.sort 
+  end 
+  new_hash
+end 
+  
+  
+  
+end
+
+
+
+
